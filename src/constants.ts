485441a718ab9d36a16d395ff95c83c3d2084b36
taayyohh <milesxbridges@gmail.com>
2023-09-08 14:18:50 -0700

    chain id env

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const OAK_VAULT_PROXY_ADDRESS = process.env
   .NEXT_PUBLIC_OAK_VAULT_PROXY as `0x${string}`
 
-export const CHAIN_ID = process.env.CHAIN_ID as unknown as number
+export const CHAIN_ID = process.env.NEXT_PUBLIC_CHAIN_ID as unknown as number
 
 export const MAX_SWAP_LIMIT = 100 * 10 ** 6 //TODO: should derive from contract
